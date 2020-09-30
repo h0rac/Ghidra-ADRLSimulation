90ffcbde8776792eae128eb5fa6a968f0d6dbf34
h0rac <grzegorz.wypych@pl.ibm.com@h0racs-MacBook-Pro-517.local>
2020-09-30 20:51:15 +0200

    Refactoring

--- SimulateADRL.java
+++ SimulateADRL.java
@@ -13,67 +13,53 @@ import ghidra.program.model.address.Address;
 public class SimulateADRL extends GhidraScript {
 
     public void run() throws Exception {
-    
     adrlSimulate();
-
    }
     
-
     private void adrlSimulate() {
         Listing listing = currentProgram.getListing();
         Address adrpAddr = null;
         Address nextAddr = null;
         Instruction instr = null;
         CodeUnit codeUnit = null;
-        
-        for (CodeUnitIterator i = listing.getCodeUnits(true); i.hasNext();) {
+        monitor.initialize(listing.getNumCodeUnits());
+        CodeUnitIterator i = listing.getCodeUnits(true);
+        while (i.hasNext() && !monitor.isCancelled()) {
         	CodeUnit temp = i.next();
-           	long adrpValue = 0;
+        	monitor.incrementProgress(1);
+        	long adrpValue = 0;
         	long nextAddrValue = 0;
-            if(temp.getMnemonicString().equals("adrp")) {
-            	adrpAddr = temp.getAddress();
-            	nextAddr = temp.getMaxAddress().add(1);
-            	
+        	if(temp.getMnemonicString().equals("adrp")) {
+        		adrpAddr = temp.getAddress();
+        		nextAddr = temp.getMaxAddress().add(1);
         		codeUnit = currentProgram.getListing().getCodeUnitAt(nextAddr);
         		if(codeUnit == null) {
         			return;
         		}
         		String addOperand = codeUnit.getMnemonicString();
+        		instr = listing.getInstructionAt(adrpAddr);
+        		if(instr == null) {
+        			return;
+        		}
+        		adrpValue = Long.decode(instr.getOpObjects(1)[0].toString());
         		if (addOperand.equals("add")) {
-        			int numOperands = codeUnit.getNumOperands();
-        			int adrpOperands = temp.getNumOperands();
-                	println("Address: "+ adrpAddr);
-                	println("ARM64 mnemonic 1: "+ temp.getMnemonicString());
-            		println("ARM64 mnemonic 1 operands number: "+adrpOperands);
-                	println("Next Addr: "+nextAddr);
-        			println("ARM64 mnemonic 2: "+addOperand);
-        			println("ARM64 mnemonic 2 operands number: "+numOperands);
-                	instr = listing.getInstructionAt(adrpAddr);
-                	if(instr == null) {
-                		return;
-                	}
-                    adrpValue = Long.decode(instr.getOpObjects(1)[0].toString());
-                    println("adrp value: "+Long.toHexString(adrpValue));
-                	instr = listing.getInstructionAt(nextAddr);
-                	if(instr == null) {
-                		return;
-                	}
-                	nextAddrValue = Long.decode(instr.getOpObjects(2)[0].toString());
-                	println("add value: "+Long.toHexString(nextAddrValue));
-              
+        			println("ADRP mnemonic address: "+ adrpAddr);
+        			instr = listing.getInstructionAt(nextAddr);
+        			if(instr == null) {
+        				return;
+        			}
+        			nextAddrValue = Long.decode(instr.getOpObjects(2)[0].toString());
         		}else {
         			continue;
         		}
-            	long refValue = adrpValue + nextAddrValue;
-            	Address addr = parseAddress(Long.toHexString(refValue));
-            	if(addr == null) {
-            		return;
-            	}
-            	println("refValue : "+Long.toHexString(refValue));
-            	println("Reference addr : "+ addr.toString());
+        		long refValue = adrpValue + nextAddrValue;
+        		Address addr = parseAddress(Long.toHexString(refValue));
+        		if(addr == null) {
+        			return;
+        		} 
         		temp.addOperandReference(1, addr , RefType.DATA, SourceType.DEFAULT);
-        		
-            }
-         }
+
+        	}
+        }
     }
 }
